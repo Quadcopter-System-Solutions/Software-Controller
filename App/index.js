059432d45aa36db7d1c5cb9c9cc1cd7aaeff89c8
Alex <zthh626@gmail.com>
2021-01-25 00:02:53 -0800

    Fixed imports from components

--- App/index.js
+++ App/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import QuadControllerPage from '../components/Pages/QuadControllerPage/QuadControllerPage'
+
 
 const YourApp = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-        Try editing me! 🎉
-      </Text>
+      <QuadControllerPage />
     </View>
   );
 }
 
-// export default YourApp;
-export {default} from '../storybook';
+export default YourApp;
+// export {default} from '../storybook';
