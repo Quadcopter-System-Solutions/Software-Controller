b3871888b43ee0c75af4fd2d8f7af630592f3301
Alex <zthh626@gmail.com>
2021-01-24 23:54:52 -0800

    Removing ControllerApp and moving Components to same repo

---
+++ storybook/storyLoader.js
@@ -0,0 +1,27 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require("../components/Atoms/circle/circle.stories");
+  require("../components/Atoms/iconButton/iconButton.stories");
+  require("../components/Molecules/circleButtonController/circleButtonController.stories");
+  require("../components/Molecules/circleGestureController/circleGestureController.stories");
+  require("../components/Organisms/mainControllers/mainControllers.stories");
+  require("../components/Pages/QuadControllerPage/QuadControllerPage.stories");
+}
+
+const stories = [
+  "../components/Atoms/circle/circle.stories",
+  "../components/Atoms/iconButton/iconButton.stories",
+  "../components/Molecules/circleButtonController/circleButtonController.stories",
+  "../components/Molecules/circleGestureController/circleGestureController.stories",
+  "../components/Organisms/mainControllers/mainControllers.stories",
+  "../components/Pages/QuadControllerPage/QuadControllerPage.stories",
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
